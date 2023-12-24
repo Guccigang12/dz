0deddde8ee30c8efe5fc1fda838fa38eaeaa4ded
Guccigang12 <38140592+Guccigang12@users.noreply.github.com>
2023-12-24 15:48:00 +0200

    Create dz.py

---
+++ dz.py
@@ -0,0 +1,20 @@
+import sqlite3
+conn = sqlite3.connect('data1.db')
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS People (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT,
+                    age INTEGER
+                )''')
+people_data = [
+    ('Timyr', 11),
+    ('Artyr', 11),
+    ('Myrmyr', 11)
+]
+cursor.executemany('INSERT INTO People (name, age) VALUES (?, ?)', people_data)
+conn.commit()
+cursor.execute('SELECT * FROM People')
+ryadoks = cursor.fetchall()
+for ryadok in ryadoks:
+    print(ryadok)
+conn.close()
